826fccb24980dc7e6808a0814d5cfffdd0550b69
salaBD <teolealsabogal@gmail.com>
2016-04-09 13:05:11 -0500

    python

---
+++ cualquier nombre.py
@@ -0,0 +1,30 @@
+print "elija"
+print "1 si desea calculadora 2 saber si es par o impar"
+val1 = int (raw_input ("elija"))
+if val1 == 1:
+    print "calculadora"
+    print "1=suma 2=resta 3=multiplicacion 4=division"
+    val45 = int (raw_input ("elija el operador deseado"))
+    if val45 == 1:
+        print "suma"
+        var1 = int (raw_input ("escriba un numero"))
+        var2 = int (raw_input ("escriba un numero"))
+        print var1 + var2
+    val25 = int (raw_input ("elija el operador deseado"))
+    if val25 == 2:
+        print "resta"
+        var3 = int (raw_input ("escriba un numero"))
+        var4 = int (raw_input ("escriba un numero"))
+        print var3 - var4
+     val35 = int (raw_input ("elija el operador deseado"))
+     if val35 == 3:
+         print "multiplicacion"
+         var5 = int (raw_input ("escriba un numero"))
+         var6 = int (raw_input ("escriba un numero"))
+         print var5 * var6
+
+
+    
+        
+    
+    
